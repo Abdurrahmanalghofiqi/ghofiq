3a54b70e64d6cb4e6fc70cb7556442691b5f56a6
Abdurrahmanalghofiqi <abdurrahmanalghofiqi69@gmail.com>
2023-03-06 15:48:48 +0700

    first commit

---
+++ c+2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main () {//begin
+
+//numeric p,l,luas;
+double p,l,luas;
+//display "masukan panjang"
+cout<<"masukkan panjang= ";
+//accept p
+cin>>p;
+//display "masukkan Lebar"
+cout<<"masukkan lebar=";
+//accept L
+cin>>l;
+//compute luas=p*L
+luas=p*l;
+//display "luas persegi panjang adalah",luas
+cout<<"luas persegi panjang adalah"<<luas;
+
+}//end
